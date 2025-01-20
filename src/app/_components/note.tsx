839028274ab67430c5b537e3ba778bf7f47f7435
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2025-01-20 17:44:15 +0100

    fix: minor refactor of notes component

--- src/app/_components/note.tsx
+++ src/app/_components/note.tsx
@@ -4,29 +4,32 @@ import { useState } from "react";
 
 import { api } from "~/trpc/react";
 
-export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
+export function LatestNote() {
+  const [latestNote] = api.note.getLatest.useSuspenseQuery();
 
   const utils = api.useUtils();
+
   const [name, setName] = useState("");
-  const createPost = api.post.create.useMutation({
+  const [content, setContent] = useState("");
+
+  const createNote = api.note.create.useMutation({
     onSuccess: async () => {
-      await utils.post.invalidate();
+      await utils.note.invalidate();
       setName("");
     },
   });
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      {latestNote ? (
+        <p className="truncate">Your most recent post: {latestNote.name}</p>
       ) : (
-        <p>You have no posts yet.</p>
+        <p>You have no notes yet.</p>
       )}
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          createPost.mutate({ name });
+          createNote.mutate({ name, content });
         }}
         className="flex flex-col gap-2"
       >
@@ -37,12 +40,20 @@ export function LatestPost() {
           onChange={(e) => setName(e.target.value)}
           className="w-full rounded-full px-4 py-2 text-black"
         />
+
+        <textarea
+          placeholder="Content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full rounded-full px-4 py-2 text-black"
+        />
+
         <button
           type="submit"
           className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          disabled={createPost.isPending}
+          disabled={createNote.isPending}
         >
-          {createPost.isPending ? "Submitting..." : "Submit"}
+          {createNote.isPending ? "Submitting..." : "Submit"}
         </button>
       </form>
     </div>
